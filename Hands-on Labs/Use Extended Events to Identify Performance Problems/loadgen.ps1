38ce11a7c56cfb197a07a7197668545560ff482f
Landon Fowler <landon.fowler@linuxacademy.com>
2020-10-05 12:18:02 -0400

    testing loadgen script

---
+++ Hands-on Labs/Use Extended Events to Identify Performance Problems/loadgen.ps1
@@ -0,0 +1,12 @@
+###################################################################
+# Source: A Cloud Guru Azure Database Administrator Associate Course
+# Author: Landon Fowler
+# Purpose: Script to generate load against Azure SQL Database
+# Date Updated: 10/05/2020
+###################################################################
+
+# Iterator
+$i = 1
+
+# Function to insert rows and select large amounts of data
+Write-Host "the script works"
