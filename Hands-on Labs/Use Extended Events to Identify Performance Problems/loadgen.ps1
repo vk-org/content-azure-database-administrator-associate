ae44e0af1880000dec6592a95666cab0e77e5f45
Landon Fowler <landon.fowler@linuxacademy.com>
2020-10-05 13:19:55 -0400

    Update loadgen script

--- Hands-on Labs/Use Extended Events to Identify Performance Problems/loadgen.ps1
+++ Hands-on Labs/Use Extended Events to Identify Performance Problems/loadgen.ps1
@@ -1,12 +1,31 @@
-###################################################################
+####################################################################
 # Source: A Cloud Guru Azure Database Administrator Associate Course
 # Author: Landon Fowler
 # Purpose: Script to generate load against Azure SQL Database
 # Date Updated: 10/05/2020
-###################################################################
+####################################################################
+
+# Parameters
+param (
+    $instance
+)
+
+# Variables
+$database = "acweb"
+$query1 = "INSERT INTO SalesLT.Customer (FirstName, LastName, CompanyName, EmailAddress, PasswordHash, PasswordSalt)
+VALUES ('Delmar', 'Database', 'A Cloud Guru', 'delmar@acloud.guru', 'L/Rlwxzp4w7RWmEgXX+/A7cXaePEPcp+KwQhl2fJL7w=', '1KjXYs4=')"
+$query2 = "SELECT * FROM SalesLT.ProductDescription"
 
 # Iterator
 $i = 1
 
 # Function to insert rows and select large amounts of data
-Write-Host "the script works"
+function run_sql {
+    Invoke-SQLCMD -ServerInstance $instance -Database $database -Query $query1 -Username delmar -Password "AwesomePassword!"
+    Invoke-SQLCMD -ServerInstance $instance -Database $database -Query $query2 -Username delmar -Password "AwesomePassword!"
+}
+
+# Infinitely loop and call the run_sql function
+while ($i -lt 2) {
+    run_sql
+}
